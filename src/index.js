675ecc9a8ebdc4551f6b4640f8bcd03e2907fb83
Ivan <hammer.99.ivan@gmail.com>
2022-05-02 01:01:53 +0700

    add mongosh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,73 @@
 const express = require('express');
-const app = express();
+const { ApolloServer, gql } = require('apollo-server-express');
+require('dotenv').config();
+const db = require('./db');
+const models = require('./models');
+
+
+// Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
-app.get('/', (req, res) => res.send('test before breakfast'));
-app.listen(port, () =>
- console.log(`Server running at http://localhost:${port}`)
+const DB_HOST = process.env.DB_HOST;
+
+
+let notes = [
+    { id: '1', content: 'Note one', author: 'Ivan Koma' },
+    { id: '2', content: 'Записка два', author: 'Doctor Who' },
+    { id: '3', content: 'Seven Pillars of Wisdom', author: 'Lowrens' }
+   ];
+   
+
+// Construct a schema, using GraphQL's schema language
+const typeDefs = gql`
+ type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+    }
+ type Note {
+    id: ID!
+    content: String!
+    author: String!
+    }
+ type Mutation {
+     newNote(content: String!): Note!
+    }
+`;
+// Provide resolver functions for our schema fields
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+        notes: async () => {
+            return await models.Note.find();
+           },        
+           note: async (parent, args) => {
+            return await models.Note.findById(args.id);
+           }           
+    },
+    Mutation: {
+        newNote: async (parent, args) => {
+            return await models.Note.create({
+            content: args.content,
+            author: 'Koma'
+            });
+           }
+           
+       }
+   };
+const app = express();
+
+db.connect(DB_HOST);
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+app.listen({ port }, () =>
+ console.log(
+ `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+ )
 );
+
+
+
+
---
+++ src/models/index.js
@@ -0,0 +1,5 @@
+const Note = require('./note');
+const models = {
+ Note
+};
+module.exports = models;
